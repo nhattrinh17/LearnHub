d0ae38f2dd5ca37a847f7db693e576621b317fad
nhattrinh17 <trinhminhnhatxt123@gmail.com>
2024-08-01 22:43:23 +0700

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,43 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ts',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+          '.svg',
+          '.jpg',
+          '.png',
+        ],
+        alias: {
+          '~': './src',
+          // '@modules': './src/modules',
+          // '@assets': './src/assets',
+          // '@uiCore': './src/uiCore',
+          // '@constants': './src/constants',
+          // '@utils': './src/utils',
+          // '@redux': './src/redux',
+          // '@models': './src/models',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+    // ['react-native-reanimated/plugin'],
+  ],
 };
